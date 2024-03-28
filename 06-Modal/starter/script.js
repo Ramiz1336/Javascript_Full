f32956ef1e66b317799958841cdf01fe5380b591
StarkRamiz <starkramizsk@gmail.com>
2024-03-29 04:30:12 +0530

    modal done

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,3 +1,36 @@
 'use strict';
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnOpenModal);
 
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  console.log(btnOpenModal[i].addEventListener('click', openModal));
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if(e.key === 'Escape')
+  {
+    if(!modal.classList.contains('hidden')){
+        closeModal();
+    }
+  }
+});
